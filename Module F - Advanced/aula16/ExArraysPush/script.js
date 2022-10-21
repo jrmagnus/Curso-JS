54c3f6f714949a0dcfa6d66ab4d5a8a4cf931163
Junior Magnus <juninhu.freerider@gmail.com>
2022-10-20 22:56:05 -0300

    Input numbers ok, list ok, order ok

--- Module F - Advanced/aula16/ExArraysPush/script.js
+++ Module F - Advanced/aula16/ExArraysPush/script.js
@@ -1,31 +1,80 @@
+let number = document.getElementById('numberTextInput')
+let logBox = document.getElementById('numberList')
+let result = document.getElementById('result')
+let values = []
+
+number.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        inputNumber()
+      }
+})
+
 function inputNumber() {
-    
-    let values = []
-    let number = Number(document.getElementById('numberTextInput').value)
-    let logBox = document.getElementById('numberList')
-
-    function isNumber(n) {
-        if ( n > 0 && n < 100 ) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    function inList (n, l) {
-        if ( l.indexOf(number) != -1 ) {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    if ( !isNumber(number) || inList(number, values) ) {
-        alert("Invalid value or allready addedd")
+    if ( !isNumber(number.value) || inList(number.value, values) ) {
+        alert(`Invalid value or allready addedd`)
     } else {
-        let item = document.createElement('logNumber')
-        item.text = `Input number ${number}`
+        addNumber()
+    }
+    number.value = ''
+    number.focus()
+}
+
+function isNumber(n) {
+    if ( Number(n) > 0 && Number(n) < 100 ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList (n, l) {
+    if ( l.indexOf(Number(n)) != -1 ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function addNumber() {
+    values.push(Number(number.value))
+    let item = document.createElement('option')
+    item.text = `Input number ${number.value}`
+    item.value = `logNumber${values.length}`
+    logBox.appendChild(item)
+}
+
+function calcAndOrder() {
+    orderValues()
+    setMaxMin()
+    valuesSum()
+    inputResult()
+}
+
+function orderValues() {
+    values.sort(function(a, b) {
+        return a - b
+    })
+    logBox.innerHTML = ''
+    for ( let i = 0; i < values.length ; i++) {
+        let item = document.createElement('option')
+        item.text = `Input number ${values[i]}`
         item.value = `logNumber${i}`
         logBox.appendChild(item)
     }
+}
+
+function setMaxMin () {
+    let maxValue = ''
+    let minValue = ''
+}
+
+function valuesSum () {
+    let valuesSum = 0
+    for( i = 0; i < values.length; i++ ) {
+        valuesSum += values[i]
+    }
+}
+
+function inputResult() {
+    //todo
 }
